3fe6c2d97621a51a7b5a6e8bcb9efd6d901d7ae9
Ankur Jaiswal <ankujais@adobe.com>
2024-01-10 13:17:00 +0530

    Moving functions to a common file. Checking tags. Not checking Port and Protocol

--- checkov/terraform/checks/resource/aws/EC2WithAdminLikeAccess.py
+++ checkov/terraform/checks/resource/aws/EC2WithAdminLikeAccess.py
@@ -1,10 +1,13 @@
 import re
-import ipaddress
-from typing import List
+
+from typing import List, Union
+
+from igraph import VertexSeq
 
 from checkov.common.models.enums import CheckResult, CheckCategories
 from checkov.terraform.checks.resource.base_resource_check import BaseResourceCheck
-from checkov.terraform.checks.resource.aws.EC2WithAccessFromInternet import flatten, flatten_cidr_blocks, is_public_ip
+from checkov.terraform.checks.resource.aws.iac_common import flatten, is_public_ip,\
+    get_sg_ingress_attributes, is_tagged_for_exceptions, contains_exception_tag
 
 AWS = 'aws'
 
@@ -15,18 +18,6 @@ AWS_IAM_ROLE = 'aws_iam_role'
 AWS_IAM_ROLE_POLICY = 'aws_iam_role_policy'
 AWS_IAM_ROLE_POLICY_ATTACHMENT = 'aws_iam_role_policy_attachment'  # AWS managed policies
 AWS_SECURITY_GROUP = 'aws_security_group'
-
-PUBLIC_PORT_TAG_KEYS = [
-    "Adobe:PublicPorts",
-    "Adobe.PublicPorts",
-    "Adobe-PublicPorts"
-]
-
-JUSTIFICATION_TAG_KEYS = [
-    "Adobe:PortJustification",
-    "Adobe.PortJustification",
-    "Adobe-PortJustification"
-]
 
 
 def get_actions_from_statement(statement):
@@ -119,87 +110,19 @@ def get_admin_actions(policy):
     return admin_actions
 
 
-def generate_tagged_exceptions(tags: dict):
-    # print("Generating tagged exceptions")
+def _is_ec2_instance_publicly_accessible(graph, resource_instance: VertexSeq, resource_instance_type: str) -> Union[bool, None]:
+    """
+    Check if the EC2 instance is publicly accessible based on its connected security groups.
 
-    publicPortString = ""
-    publicPortJustification = "None Specified"
-    exceptions = {}
-    exceptionTagKey = ""
+    Parameters:
+    - graph (Graph): The igraph Graph object.
+    - aws_instance (VertexSeq): VertexSeq for the EC2 Instance.
 
-    for k in PUBLIC_PORT_TAG_KEYS:
-        if k in tags:
-            publicPortString = str(tags[k])
-            exceptionTagKey = k
-            break
+    Returns:
+    - Union[bool, None]: True if the EC2 instance is publicly accessible, None otherwise.
+    """
 
-    for k in JUSTIFICATION_TAG_KEYS:
-        if k in tags:
-            publicPortJustification = str(tags[k])
-            break
-
-    publicPortsStringList = publicPortString.split(",")
-    for portstring in publicPortsStringList:
-        port = None
-        proto = None
-        range_list = []
-        if portstring:
-            # re.search returns Nonetype upon a non-match
-            # changing code structure to using if-else statements, no longer need exception catching
-            portstring = portstring.strip()
-            if re.search("^(TCP|UDP)?\ *(\d+\ *-\ *\d+|\d+)$", portstring):  # noqa: W605
-                proto = re.search("TCP|UDP", portstring)
-                if proto:
-                    proto = proto.group(0)
-                else:
-                    proto = "TCP"
-
-                port_range = re.search("\d+\ *\-\ *\d+$", portstring)  # noqa: W605
-                if port_range:
-                    port_range = port_range.group(0)
-                    port_range = port_range.split("-")
-                    port_range = [p.strip() for p in port_range]
-                    if int(port_range[0]) > int(port_range[1]):
-                        print(f"Invalid port range {port_range[0]} to {port_range[1]}")
-                    else:
-                        range_list = [str(i) for i in range(int(port_range[0]), int(port_range[1]) + 1)]
-                else:
-                    print(f"Missing port range in {portstring}")
-                    print("Finding single port")
-                    portstring = portstring.strip()
-
-                    port = re.search("\d+$", portstring)  # noqa: W605
-                    if port:
-                        port = port.group(0)
-                    else:
-                        print(f"Missing port number in {portstring}")
-            else:
-                print(f"Malformed input {portstring}")
-                continue
-
-        if port:
-            proto_port = "%s%s" % (proto, port)
-            exceptions[proto_port] = {
-                "justification": publicPortJustification,
-                "exceptionSource": exceptionTagKey,
-                "port": port
-            }
-        elif range_list:
-            for port in range_list:
-                proto_port = "%s%s" % (proto, port)
-                exceptions[proto_port] = {
-                    "justification": publicPortJustification,
-                    "exceptionSource": exceptionTagKey,
-                    "port": port
-                }
-    return exceptions
-
-
-def is_ec2_instance_publicly_accessible(graph, aws_instance):
-    aws_instance_attributes = aws_instance.attributes()
-    aws_instance_name = aws_instance_attributes['attr']['block_name_'].split('.')[1]
-
-    connected_security_groups = [neighbor for neighbor in graph.vs[aws_instance.index].neighbors() if
+    connected_security_groups = [neighbor for neighbor in graph.vs[resource_instance.index].neighbors() if
                                  neighbor['resource_type'] == AWS_SECURITY_GROUP]
 
     for security_group in connected_security_groups:
@@ -213,46 +136,23 @@ def is_ec2_instance_publicly_accessible(graph, aws_instance):
         ingress_list = flatten(ingress_list)
 
         for ingress in ingress_list:
-            # cidr_blocks = ingress.get('cidr_blocks', [None])[0]
-            cidr_blocks = ingress.get('cidr_blocks')
-            if cidr_blocks:
-                cidr_blocks = flatten_cidr_blocks(cidr_blocks)
-            else:
-                continue  # no cidr blocks, cannot check
+            ingress_attributes = get_sg_ingress_attributes(ingress)
 
-            from_port = ingress.get('from_port')
-            if from_port:
-                if isinstance(from_port, list):
-                    from_port = from_port[0]
+            cidr_blocks = ingress_attributes.get('cidr_blocks', None)
+            # from_port = ingress_attributes.get('from_port', None)
+            # to_port = ingress_attributes.get('to_port', None)
+            # protocol = ingress_attributes.get('protocol', None)
 
-            to_port = ingress.get('to_port')
-            if to_port:
-                if isinstance(to_port, list):
-                    to_port = to_port[0]
-
-            protocol = ingress.get('protocol', 'TCP')
-            if protocol:
-                if isinstance(protocol, list):
-                    protocol = protocol[0]
-                    if isinstance(protocol, str):
-                        protocol = protocol.upper()
-
-            # from_port = ingress.get('from_port', [None])[0]
-            # to_port = ingress.get('to_port', [None])[0]
-            # protocol = ingress.get('protocol', ['TCP'])[0].upper() if 'protocol' in ingress else None
+            if not cidr_blocks:
+                continue
 
             for ip_or_cidr in cidr_blocks:
                 if is_public_ip(ip_or_cidr):
                     return True
-            # if '0.0.0.0/0' in cidr_blocks:
-            #     aws_instance_tags = aws_instance_attributes['attr']['config_']['aws_instance'][aws_instance_name]['tags']
-            #     if aws_instance_tags and isinstance(aws_instance_tags, list):
-            #         aws_instance_tags = aws_instance_tags[0]
-            #         tagged_exceptions = generate_tagged_exceptions(aws_instance_tags)
-            #
-            #         for port in range(int(from_port), int(to_port) + 1):
-            #             if f"{protocol}{from_port}" not in tagged_exceptions:
-            #                 return True
+                    # if is_tagged_for_exceptions(resource_instance, resource_instance_type, from_port, to_port, protocol):
+                    #     continue
+                    # else:
+                    #     return True
 
 
 class EC2WithAdminLikeAccess(BaseResourceCheck):
@@ -299,7 +199,7 @@ class EC2WithAdminLikeAccess(BaseResourceCheck):
         for aws_instance in aws_instance_list:
 
             # First check if aws_instance is publicly accessible
-            is_publicly_accessible = is_ec2_instance_publicly_accessible(graph, aws_instance)
+            is_publicly_accessible = _is_ec2_instance_publicly_accessible(graph, aws_instance, AWS_INSTANCE)
 
             if not is_publicly_accessible:
                 continue
@@ -312,10 +212,11 @@ class EC2WithAdminLikeAccess(BaseResourceCheck):
             for iam_role in connected_iam_roles:
                 connected_iam_custom_policies = [neighbor for neighbor in graph.vs[iam_role.index].neighbors() if
                                                  neighbor['resource_type'] == AWS_IAM_ROLE_POLICY]
+
+                # For iam policy, we need to first describe the policy then check it's JSON. Cannot be done without making call.to AWS account
+                # Cannot be done
                 connected_iam_aws_policies = [neighbor for neighbor in graph.vs[iam_role.index].neighbors() if
                                               neighbor['resource_type'] == AWS_IAM_ROLE_POLICY_ATTACHMENT]
-                print(connected_iam_custom_policies)
-                print(connected_iam_aws_policies)
 
                 for policy in connected_iam_custom_policies:
                     policy_attributes = policy.attributes()
@@ -325,9 +226,10 @@ class EC2WithAdminLikeAccess(BaseResourceCheck):
                     has_admin_actions = get_admin_actions(
                         policy_content_json[0])  # todo aj check if more than 1 item can be present in the dict
                     if has_admin_actions:
-                        return CheckResult.FAILED
-
-                # todo aj for iam policy, we need to first describe the policy then check it's JSON. Do it later
+                        if contains_exception_tag(aws_instance, AWS_INSTANCE, tag_key="AllowPublicAdminLike", tag_value=True):
+                            continue
+                        else:
+                            return CheckResult.FAILED
 
         return result
 
