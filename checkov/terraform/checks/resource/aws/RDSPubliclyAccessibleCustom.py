ddebbbab17942455d0863c028fc3933c0acf983c
Ankur Jaiswal <ankujais@adobe.com>
2024-01-11 12:40:00 +0530

    remove unused code

--- checkov/terraform/checks/resource/aws/RDSPubliclyAccessibleCustom.py
+++ checkov/terraform/checks/resource/aws/RDSPubliclyAccessibleCustom.py
@@ -1,21 +1,27 @@
 import ipaddress
 
+from igraph import Vertex, Graph
+
 from checkov.common.models.enums import CheckResult, CheckCategories
 from checkov.terraform.checks.resource.base_resource_check import BaseResourceCheck
-from checkov.terraform.checks.resource.aws.EC2WithAccessFromInternet import flatten, flatten_cidr_blocks, is_public_ip
+from checkov.terraform.checks.resource.aws.iac_common import flatten, is_public_ip, get_sg_ingress_attributes
 
 AWS_DB_INSTANCE = 'aws_db_instance'
 AWS_RDS_CLUSTER_INSTANCE = 'aws_rds_cluster_instance'
 AWS_SECURITY_GROUP = 'aws_security_group'
 
 
-def is_rds_publicly_accessible(graph, aws_rds):
+def _is_rds_publicly_accessible(graph: Graph, aws_rds: Vertex):
+    """
+    Check if the RDS is publicly accessible
+    :param graph: graph instance
+    :param aws_rds: aws_rds vertex
+    :return: True if the RDS is publicly accessible, False otherwise
+    """
     aws_rds_attributes = aws_rds.attributes()
 
     if aws_rds_attributes['attr'].get('publicly_accessible'):
         return True
-
-    aws_rds_name = aws_rds_attributes['attr']['block_name_'].split('.')[1]
 
     connected_security_groups = [neighbor for neighbor in graph.vs[aws_rds.index].neighbors() if
                                  neighbor['resource_type'] == AWS_SECURITY_GROUP]
@@ -23,7 +29,8 @@ def is_rds_publicly_accessible(graph, aws_rds):
     for security_group in connected_security_groups:
         security_group_attributes = security_group.attributes()
         security_group_name = security_group_attributes['attr']['block_name_'].split('.')[1]
-        ingress_list = security_group_attributes['attr']['config_']['aws_security_group'][security_group_name].get('ingress')
+        ingress_list = security_group_attributes['attr']['config_'][AWS_SECURITY_GROUP][security_group_name].get(
+            'ingress')
 
         if not ingress_list:
             continue  # no ingress_list, cannot check
@@ -31,46 +38,18 @@ def is_rds_publicly_accessible(graph, aws_rds):
         ingress_list = flatten(ingress_list)
 
         for ingress in ingress_list:
-            # cidr_blocks = ingress.get('cidr_blocks', [None])[0]
-            cidr_blocks = ingress.get('cidr_blocks')
-            if cidr_blocks:
-                cidr_blocks = flatten_cidr_blocks(cidr_blocks)
-            else:
-                continue  # no cidr blocks, cannot check
+            ingress_attributes = get_sg_ingress_attributes(ingress)
+            cidr_blocks = ingress_attributes.get('cidr_blocks', None)
+            # from_port = ingress_attributes.get('from_port', None)
+            # to_port = ingress_attributes.get('to_port', None)
+            # protocol = ingress_attributes.get('protocol', None)
 
-            from_port = ingress.get('from_port')
-            if from_port:
-                if isinstance(from_port, list):
-                    from_port = from_port[0]
-
-            to_port = ingress.get('to_port')
-            if to_port:
-                if isinstance(to_port, list):
-                    to_port = to_port[0]
-
-            protocol = ingress.get('protocol', 'TCP')
-            if protocol:
-                if isinstance(protocol, list):
-                    protocol = protocol[0]
-                    if isinstance(protocol, str):
-                        protocol = protocol.upper()
-
-            # from_port = ingress.get('from_port', [None])[0]
-            # to_port = ingress.get('to_port', [None])[0]
-            # protocol = ingress.get('protocol', ['TCP'])[0].upper() if 'protocol' in ingress else None
+            if not cidr_blocks:
+                continue
 
             for ip_or_cidr in cidr_blocks:
                 if is_public_ip(ip_or_cidr):
                     return True
-            # if '0.0.0.0/0' in cidr_blocks:
-            #     aws_instance_tags = aws_instance_attributes['attr']['config_']['aws_instance'][aws_instance_name]['tags']
-            #     if aws_instance_tags and isinstance(aws_instance_tags, list):
-            #         aws_instance_tags = aws_instance_tags[0]
-            #         tagged_exceptions = generate_tagged_exceptions(aws_instance_tags)
-            #
-            #         for port in range(int(from_port), int(to_port) + 1):
-            #             if f"{protocol}{from_port}" not in tagged_exceptions:
-            #                 return True
 
 
 class RDSPubliclyAccessibleCustom(BaseResourceCheck):
@@ -90,16 +69,17 @@ class RDSPubliclyAccessibleCustom(BaseResourceCheck):
             return result
 
         aws_rds_list = graph.vs.select(lambda vertex: vertex['attr'].get('__address__') == conf["__address__"] and (
-                                                      vertex["resource_type"] == AWS_DB_INSTANCE or
-                                                      vertex["resource_type"] == AWS_RDS_CLUSTER_INSTANCE
-                                       ))
+                vertex["resource_type"] == AWS_DB_INSTANCE or
+                vertex["resource_type"] == AWS_RDS_CLUSTER_INSTANCE
+        ))
 
         for aws_rds in aws_rds_list:
-            is_publicly_accessible = is_rds_publicly_accessible(graph, aws_rds)
+            is_publicly_accessible = _is_rds_publicly_accessible(graph, aws_rds)
 
             if is_publicly_accessible:
                 return CheckResult.FAILED
 
         return result
+
 
 check = RDSPubliclyAccessibleCustom()
